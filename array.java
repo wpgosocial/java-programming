2931300060566ca550d588e92d07a01ea94cff27
wpgosocial <romeoclen3@gmail.com>
2023-05-01 19:07:51 -0400

    array

--- array.java
+++ array.java
@@ -2,9 +2,12 @@ import java.util.ArrayList;
 
 public class Main {
 public static void main(String[] args) {
-    
+
     ArrayList<String> cars = new ArrayList<String>();
     cars.add("BMW");
     cars.add("Ford");
+    cars.add("Acura");
+    cars.add("Audi");
+    cars.add("Tesla");
 }
 }

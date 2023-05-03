3d5e2046a6fbca8d2906c826ca30de35f9504932
wpgosocial <romeoclen3@gmail.com>
2023-05-03 17:30:27 -0400

    switch array

--- switch.java
+++ switch.java
@@ -3,16 +3,6 @@ import java.util.ArrayList;
 public class Main {
 public static void main(String[] args) {
 
-    ArrayList<String> cars = new ArrayList<String>();
-    cars.add("BMW");
-    cars.add("Ford");
-    
-    cars.add("Acura");
-    cars.add("Audi");
-
-    cars.add("Tesla");
-}
-
 
 
 int day = 2;
@@ -32,4 +22,5 @@ case
 break
 ;
 }
+
 }

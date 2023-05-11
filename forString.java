3d799c3d6f024a9dfae349e02dc83181f976da54
wpgosocial <romeoclen3@gmail.com>
2023-05-11 02:07:27 -0400

    forString

---
+++ forString.java
@@ -0,0 +1,13 @@
+public class forString {
+
+public static void main(String[] args){
+    String[] cars = {"Volvo", "BMW", "Ford"};
+for
+ (String i : 
+cars
+) {
+  System.out.println(i);
+}
+    
+}
+}

9356b969063d41dfd695af27c0986acac3c15c18
wpgosocial <romeoclen3@gmail.com>
2023-06-06 00:12:14 -0400

    loop

--- whileLoop.java
+++ whileLoop.java
@@ -1,13 +1,13 @@
 public class whileLoop {
 
     public static void main(String[] args) {
-int i = 0;
+int x = 0;
 while
  (i < 10) {
     
-  System.out.println(i);
+  System.out.println(x);
   
-i++
+x++
 ;
 }
     }

2b083db384f37d79e87188e78f5b25a6b14f30a3
wpgosocial <romeoclen3@gmail.com>
2023-06-20 08:31:21 -0400

    Array

--- array.java
+++ array.java
@@ -6,10 +6,8 @@ public static void main(String[] args) {
     ArrayList<String> cars = new ArrayList<String>();
     cars.add("BMW");
     cars.add("Ford");
-    
     cars.add("Acura");
     cars.add("Audi");
-
     cars.add("volvo");
     cars.add("SVR");
 }

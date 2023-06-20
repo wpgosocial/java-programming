370162cac2d4df454aeb57c255643c2782baf3ba
wpgosocial <romeoclen3@gmail.com>
2023-06-19 20:29:49 -0400

    array

--- array.java
+++ array.java
@@ -11,25 +11,8 @@ public static void main(String[] args) {
     cars.add("Audi");
 
     cars.add("volvo");
+    cars.add("SVR");
 }
 
 
-
-int day = 2;
-switch (
-day
-) {
-  
-case
- 1:
-    System.out.println("Saturday");
-    break;
-  
-case
- 2:
-    System.out.println("Sunday");
-    
-break
-;
-}
 }
